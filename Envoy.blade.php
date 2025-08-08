44791cb7309d19a8688fa84b678b4a4c00266d74
Leonardo Lemos <leonardo.lemos@convenia.com.br>
2025-08-07 21:04:45 -0300

    try ci/cd

---
+++ Envoy.blade.php
@@ -0,0 +1,22 @@
+@servers(['web' => ['ubuntu@54.197.38.215']])
+ 
+@story('deploy')
+    update-code
+    install-dependencies
+	run-migrations
+@endstory
+ 
+@task('update-code')
+    cd lab
+    git pull origin main
+@endtask
+ 
+@task('install-dependencies')
+    cd lab
+    composer install --no-dev
+@endtask
+
+@task('run-migrations')
+    cd lab
+    php artisan migrate --force
+@endtask
